7280b0bf27a5b54a4840c3333f7cfcaceaaa0aef
Laura Beatris <laurabeatriserafim@gmail.com>
2019-08-10 18:40:15 -0300

    signin validation

--- components/header.js
+++ components/header.js
@@ -18,14 +18,14 @@ function Header() {
                 x2="64"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop stop-color="#fc8181" offset="0%" />
-                <stop stop-color="#e53e3e" offset="100%" />
+                <stop stopColor="#fc8181" offset="0%" />
+                <stop stopColor="#e53e3e" offset="100%" />
               </linearGradient>
             </defs>
             <path
               d="M16 25.6c2.133-8.533 7.467-12.8 16-12.8 12.8 0 14.4 9.6 20.8 11.2 4.267 1.067 8-.533 11.2-4.8C61.867 27.733 56.533 32 48 32c-12.8 0-14.4-9.6-20.8-11.2-4.267-1.067-8 .533-11.2 4.8zM0 44.8C2.133 36.267 7.467 32 16 32c12.8 0 14.4 9.6 20.8 11.2 4.267 1.067 8-.533 11.2-4.8-2.133 8.533-7.467 12.8-16 12.8-12.8 0-14.4-9.6-20.8-11.2-4.267-1.067-8 .533-11.2 4.8z"
               fill="url(#A)"
-              fill-rule="evenodd"
+              fillRule="evenodd"
             />
           </svg>
           <p className="font-bold text-red-500 text-xl uppercase italic">
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,22 +1,97 @@
+import { Component } from "react";
 import AuthLayout from "../../components/auth_layout";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
-const SignIn = () => (
-  <AuthLayout>
-    <input
-      className="focus:outline-none focus:border-red-400 p-2 m-4 border text-gray-700 border-gray-500 rounded"
-      type="email"
-      placeholder="Type your email"
-    />
-    <input
-      className="focus:outline-none focus:border-red-400 p-2 m-4 border text-gray-700 border-gray-500 rounded"
-      type="password"
-      placeholder="Type your password"
-    />
-    <button className="bg-red-400 m-4 hover:bg-red-500 border border-white px-5 py-2 text-white font-bold rounded">
-      {" "}
-      Sign in{" "}
-    </button>
-  </AuthLayout>
-);
+class SignIn extends Component {
+  state = {
+    name: "",
+    password: ""
+  };
 
+  render() {
+    return (
+      <Formik
+        initialValues={{
+          email: this.state.email,
+          password: this.state.password
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log("submitting");
+        }}
+        validationSchema={Yup.object().shape({
+          email: Yup.string()
+            .email("Invalid email")
+            .required("No email provided"),
+          password: Yup.string()
+            .required("No password provided")
+            .min(8, "Password is too short - 8 chars minimum.")
+            .matches(/(?=.*[0-9])/, "Password must contain a number.")
+        })}
+      >
+        {props => {
+          const {
+            values,
+            touched,
+            errors,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit
+          } = props;
+          return (
+            <AuthLayout>
+              <form
+                onSubmit={handleSubmit}
+                className="flex flex-col justify-center items-center"
+              >
+                <input
+                  className="focus:outline-none focus:border-red-400 p-2 m-4 border text-gray-700 border-gray-500 rounded"
+                  name="email"
+                  placeholder="Type your email"
+                  value={this.state.email}
+                  onChange={ev => {
+                    handleChange(ev);
+                    this.setState({ email: ev.target.value });
+                  }}
+                  onBlur={handleBlur}
+                />
+                {errors.email && touched.email && (
+                  <p id="error" className="text-red-500 font-bold">
+                    {errors.email}
+                  </p>
+                )}
+                <input
+                  className="focus:outline-none focus:border-red-400 p-2 m-4 border text-gray-700 border-gray-500 rounded"
+                  type="password"
+                  name="password"
+                  placeholder="Type your password"
+                  value={this.state.password}
+                  onChange={ev => {
+                    handleChange(ev);
+                    this.setState({ password: ev.target.value });
+                  }}
+                  onBlur={handleBlur}
+                />
+                {errors.password && touched.password && (
+                  <p id="error" className="text-red-500 font-bold ">
+                    {errors.password}
+                  </p>
+                )}
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="bg-red-400 m-4 hover:bg-red-500 border border-white px-5 py-2 text-white font-bold rounded"
+                >
+                  {" "}
+                  Sign in{" "}
+                </button>
+              </form>
+            </AuthLayout>
+          );
+        }}
+      </Formik>
+    );
+  }
+}
 export default SignIn;
