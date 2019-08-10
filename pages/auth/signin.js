291598d1319095243316735b76dc943dfdcb8238
Laura Beatris <laurabeatriserafim@gmail.com>
2019-08-10 18:06:28 -0300

    signup layout

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -5,16 +5,16 @@ const SignIn = () => (
     <input
       className="focus:outline-none focus:border-red-400 p-2 m-4 border text-gray-700 border-gray-500 rounded"
       type="email"
-      placeholder="Type a email"
+      placeholder="Type your email"
     />
     <input
       className="focus:outline-none focus:border-red-400 p-2 m-4 border text-gray-700 border-gray-500 rounded"
       type="password"
-      placeholder="Type a password"
+      placeholder="Type your password"
     />
-    <button className="bg-red-400 m-4 hover:bg-red-800 border border-white px-5 py-2 text-white font-bold rounded">
+    <button className="bg-red-400 m-4 hover:bg-red-500 border border-white px-5 py-2 text-white font-bold rounded">
       {" "}
-      Sign up{" "}
+      Sign in{" "}
     </button>
   </AuthLayout>
 );
--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,3 +1,27 @@
-const test = () => <p> Sign up </p>;
+import AuthLayout from "../../components/auth_layout";
 
-export default test;
+const SignUp = () => (
+  <AuthLayout>
+    <input
+      className="focus:outline-none focus:border-red-400 p-2 m-4 border text-gray-700 border-gray-500 rounded"
+      type="email"
+      placeholder="Type your email"
+    />
+    <input
+      className="focus:outline-none focus:border-red-400 p-2 m-4 border text-gray-700 border-gray-500 rounded"
+      type="password"
+      placeholder="Type your password"
+    />
+    <input
+      className="focus:outline-none focus:border-red-400 p-2 m-4 border text-gray-700 border-gray-500 rounded"
+      type="password"
+      placeholder="Type your password again"
+    />
+    <button className="bg-red-400 m-4 hover:bg-red-500 border border-white px-5 py-2 text-white font-bold rounded">
+      {" "}
+      Sign up{" "}
+    </button>
+  </AuthLayout>
+);
+
+export default SignUp;
