ebc980e30b6761580a09815e8e12ef5367302f43
Laura Beatris <laurabeatriserafim@gmail.com>
2019-08-10 17:53:09 -0300

    adding icon on header

--- components/header.js
+++ components/header.js
@@ -7,7 +7,27 @@ function Header() {
   return (
     <header className="bg-white top-0 fixed w-full">
       <div className="flex flex-wrap md:flex-no-wrap items-center justify-between max-w-4xl mx-auto p-4 md:p-8">
-        <div className="flex items-center">
+        <div className="flex flex-row items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64">
+            <defs>
+              <linearGradient
+                x1="0"
+                y1="-21.333"
+                y2="85.333"
+                id="A"
+                x2="64"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#fc8181" offset="0%" />
+                <stop stop-color="#e53e3e" offset="100%" />
+              </linearGradient>
+            </defs>
+            <path
+              d="M16 25.6c2.133-8.533 7.467-12.8 16-12.8 12.8 0 14.4 9.6 20.8 11.2 4.267 1.067 8-.533 11.2-4.8C61.867 27.733 56.533 32 48 32c-12.8 0-14.4-9.6-20.8-11.2-4.267-1.067-8 .533-11.2 4.8zM0 44.8C2.133 36.267 7.467 32 16 32c12.8 0 14.4 9.6 20.8 11.2 4.267 1.067 8-.533 11.2-4.8-2.133 8.533-7.467 12.8-16 12.8-12.8 0-14.4-9.6-20.8-11.2-4.267-1.067-8 .533-11.2 4.8z"
+              fill="url(#A)"
+              fill-rule="evenodd"
+            />
+          </svg>
           <p className="font-bold text-red-500 text-xl uppercase italic">
             befree
           </p>
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -3,12 +3,12 @@ import AuthLayout from "../../components/auth_layout";
 const SignIn = () => (
   <AuthLayout>
     <input
-      className="p-2 m-4 border text-gray-700 border-gray-500 rounded"
+      className="focus:outline-none focus:border-red-400 p-2 m-4 border text-gray-700 border-gray-500 rounded"
       type="email"
       placeholder="Type a email"
     />
     <input
-      className="p-2 m-4 border text-gray-700 border-gray-500 rounded"
+      className="focus:outline-none focus:border-red-400 p-2 m-4 border text-gray-700 border-gray-500 rounded"
       type="password"
       placeholder="Type a password"
     />
